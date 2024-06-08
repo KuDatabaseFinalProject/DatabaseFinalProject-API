fdca2136dab612dff696aa0cd3ebba98f24e91fc
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-06-09 01:49:53 +0900

    Add cors

--- src/main/kotlin/org/example/databasefinalprojectapi/movies/controller/MovieController.kt
+++ src/main/kotlin/org/example/databasefinalprojectapi/movies/controller/MovieController.kt
@@ -1,11 +1,13 @@
 package org.example.databasefinalprojectapi.movies.controller
 
 import org.example.databasefinalprojectapi.movies.service.MovieService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000/"])
 class MovieController(
     private val movieService: MovieService
 ) {
